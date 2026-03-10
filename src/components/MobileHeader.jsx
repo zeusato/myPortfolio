40bee3df58de4bce7aee872ee13e56ccb26b6578
Nguyễn Mạnh Quyết <zeusato@SHS-PTSP-QuyetNM.shs.com.vn>
2026-03-10 15:56:35 +0700

    manhquyet

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -3,7 +3,7 @@ import { HiBars3 } from 'react-icons/hi2';
 export default function MobileHeader({ onToggle }) {
     return (
         <header className="mobile-header">
-            <span className="mobile-logo">Q.N</span>
+            <span className="mobile-logo">Mạnh Quyết</span>
             <button className="hamburger-btn" onClick={onToggle}>
                 <HiBars3 />
             </button>
